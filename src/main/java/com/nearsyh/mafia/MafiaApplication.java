bd633ea15386abfa0d6f09256dcf91fb9748fb16
Nearsyh <nearsy.h@gmail.com>
2018-12-07 14:44:45 +0900

    Initialize the backend

---
+++ src/main/java/com/nearsyh/mafia/MafiaApplication.java
@@ -0,0 +1,12 @@
+package com.nearsyh.mafia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MafiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MafiaApplication.class, args);
+    }
+}
