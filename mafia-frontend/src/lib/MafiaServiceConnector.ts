db34162066f93630aadb4407c86c34f129458721
Nearsyh <nearsy.h@gmail.com>
2018-12-20 13:24:52 +0800

    Add mafia service connector

---
+++ mafia-frontend/src/lib/MafiaServiceConnector.ts
@@ -0,0 +1,42 @@
+import Axios from 'axios';
+import { Game, Event } from '@/protos/game_pb';
+
+const MAFIA_ENDPOINT = 'http://localhost:8080';
+const axios = Axios.create({
+  baseURL: MAFIA_ENDPOINT,
+  timeout: 100000,
+  responseType: 'arraybuffer',
+  headers: {
+    'Content-Type': 'application/x-protobuf',
+    'Accept': 'application/x-protobuf',
+  },
+});
+
+export async function getSupportedCharacterTypes() {
+  const response = await axios.get<string[]>(`/games/support_characters`, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    }
+  });
+  return response.data;
+}
+
+export async function createGame(charactersCount: Map<string, number>) {
+  const response = await axios.put<Uint8Array>(`/games/support_characters`, charactersCount, {
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  });
+  return Game.deserializeBinary(response.data);
+}
+
+export async function getGame(gameId: string) {
+  const response = await axios.get<Uint8Array>(`/games/${gameId}`);
+  return Game.deserializeBinary(response.data);
+}
+
+export async function updateGame(gameId: string, event: Event) {
+  const response = await axios.post<Uint8Array>(`/games/${gameId}`, event.serializeBinary());
+  return Game.deserializeBinary(response.data);
+}
